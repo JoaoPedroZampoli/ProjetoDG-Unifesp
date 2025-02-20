a042736502ab0a4e6cf7a1eb2de0add9d96de346
Luiza de Souza <souza.luiza@unifesp.br>
2025-02-20 00:12:34 -0300

    Animação de dano e morte da mosquinha.

--- Firehose/Assets/Scripts/Inimigo.cs
+++ Firehose/Assets/Scripts/Inimigo.cs
@@ -5,13 +5,23 @@ public class Inimigo : MonoBehaviour
     [SerializeField]
     private int vidas;
 
+    [SerializeField]
+    private Animator animator;
+
     public void ReceberDano()
     {
         this.vidas--;
         Debug.Log(this.name + " recebeu dano. Vida: " + this.vidas);
         if(this.vidas==0)
         {
-            GameObject.Destroy(this.gameObject);
+            //Derrotado
+            this.animator.SetBool("Morte", true);
+            GameObject.Destroy(this.gameObject, 0.6f);
+        }
+        else
+        {
+            //Recebeu dano
+            this.animator.SetTrigger("Dano");
         }
     }
 }
